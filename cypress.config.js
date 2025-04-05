17e0f8c4e3353c09b038cf4d894861273d0722a2
Triatsonbg <bojidar.garmidolov@iu-study.org>
2025-04-05 16:31:12 +0200

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-grep/src/plugin')(config);
-      return config;
     },
   },
 });
