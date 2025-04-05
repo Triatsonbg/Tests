091b0e260091c97ae109c1c8b959ae067e473642
Triatsonbg <bojidar.garmidolov@iu-study.org>
2025-04-05 16:41:27 +0200

    adding aditional tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -4,3 +4,9 @@ describe('Homepage Test', () => {
         cy.contains('Dimension'); // Adjust text to match content on your homepage
     });
 });
+describe('Homepage Intro Test', () => {
+    it('Should load the homepage and check for a Test - dev', () => {
+        cy.visit('http://localhost:8081'); // Replace with your actual dev URL
+        cy.contains('Intro'); // Adjust text to match content on your homepage
+    });
+});
--- cypress/e2e/homepageProd.cy.js
+++ cypress/e2e/homepageProd.cy.js
@@ -4,3 +4,9 @@ describe('Homepage Test', () => {
         cy.contains('Dimension'); // Adjust text to match content on your homepage
     });
 });
+describe('Homepage Intro Test', () => {
+    it('Should load the homepage and check for a Test - prod', () => {
+        cy.visit('http://localhost'); // Replace with your actual dev URL
+        cy.contains('Intro'); // Adjust text to match content on your homepage
+    });
+});
